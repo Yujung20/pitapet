5e37e57460e2e29bca433dcd4c71081c22b8c87f
eunjungL <dldmswjd322@gmail.com>
2021-11-18 16:38:42 +0000

    creae_animal.sql on delete cascade change

--- create_animal.sql
+++ create_animal.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `animal` (
     `number` int AUTO_INCREMENT,
-    `owner_id` varchar(15) NOT NULL ON DELETE CASCADE,
+    `owner_id` varchar(15) NOT NULL,
     `name` varchar(10) UNIQUE,
     `gender` varchar(5),
     `birthday` date,
     `type` varchar(10),
     `special_note` text,
     PRIMARY KEY(`number`),
-    FOREIGN KEY(`owner_id`) REFERENCES `user`(`id`)
+    FOREIGN KEY(`owner_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
 );
