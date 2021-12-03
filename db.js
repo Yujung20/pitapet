d0ad77211d68da99e5bf31a7c6be7307128f2947
eunjungL <dldmswjd322@gamil.com>
2021-12-03 17:28:33 +0000

    ignore 파일 지움

--- db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: `pit_a_pet`
-});
-db.connect();
-module.exports = db;
