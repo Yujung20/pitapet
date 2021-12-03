2b3070519cd3c96e5634cd833c38c0699a230deb
flora7883 <flora7883@gmail.com>
2021-12-03 21:23:58 +0000

    hospital_db_marker

--- hospital.js
+++ hospital.js
@@ -10,9 +10,6 @@ var db = require('./db');
 const path=require('path')
 
 function main_template(marker_list){
-    //지도(마커 전체)
-    //검색칸+검색 버튼
-    //
     return `
     <!doctype html>
     <html>
@@ -22,31 +19,64 @@ function main_template(marker_list){
         </head>
         <body> 
         <h3>검색어를 입력하세요.</h3>
+        <h5>야간 운영 병원은 붉은색 마커로 표시됩니다.</h5>
         <div id="map" style="width:700px;height:400px;"></div>
 	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=	e4410d501aa08cb040f89f52b5ee63d2"></script>
 	<script>
 		var container = document.getElementById('map');
 		var options = {
-			center: new kakao.maps.LatLng(33.450705, 126.570677),
-			level: 3
+			center: new kakao.maps.LatLng(37.59244753353966, 127.02133437301545),
+			level: 7
 		};
         var map = new kakao.maps.Map(container, options);
-        var positions = [
+        var marker_information = [
             ${marker_list}
         ];
-        var selectedMarker = null; // 클릭한 마커를 담을 변수
-            
-        for (var i = 0; i < positions.length; i ++) {
-            
+
+        var imageSrc1 = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
+            imageSize1 = new kakao.maps.Size(25, 37), // 마커이미지의 크기입니다
+            imageOption1 = {offset: new kakao.maps.Point(27, 69)};
+
+        var imageSrc2 = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다    
+            imageSize2 = new kakao.maps.Size(25, 40), // 마커이미지의 크기입니다
+            imageOption2 = {offset: new kakao.maps.Point(27, 69)};
+
+        var markerImage1 = new kakao.maps.MarkerImage(imageSrc1, imageSize1, imageOption1);
+        var markerImage2 = new kakao.maps.MarkerImage(imageSrc2, imageSize2, imageOption2);
+        for (let i = 0; i < marker_information.length; i ++) {
+            var setimage;
+            if(marker_information[i].night!=0){
+                setimage=markerImage1;
+            }
+            else{
+                setimage=markerImage2;
+            }
             // 마커를 생성합니다
             var marker = new kakao.maps.Marker({
-                map: map,
-                position: positions[i].latlng,
-                title : positions[i].title,
+                position: marker_information[i].latlng,
+                title : marker_information[i].title,
+                image: setimage,
             });
+            var infowindow= new kakao.maps.InfoWindow({
+                content: marker_information[i].content, // 인포윈도우에 표시할 내용
+            });
+            
+            marker.setMap(map)
+
+            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+        }
+        function makeOverListener(map, marker, infowindow) {
+            return function() {
+                infowindow.open(map, marker);
+            };
+        }
+        function makeOutListener(infowindow) {
+            return function() {
+                infowindow.close();
+            };
         }
 	</script>
-            
         </body>
     </html>
     `;
@@ -54,18 +84,57 @@ function main_template(marker_list){
 }
 app.get('/', function (req, res) {
     var marker_list=``;
-    db.query(`SELECT * FROM hospital `,
+    db.query(`SELECT * FROM hospital LEFT JOIN hospital_pet ON hospital.hospital_name=hospital_pet.hospital_name LEFT JOIN hospital_time ON hospital.hospital_name=hospital_time.hospital_name; `,
     function(err,hospitals){
         if (err) throw err;
         else{
-            for (var i=0; i<Object.keys(hospitals).length;i++){
-                marker_list+=`{
-                    title: '${hospitals[i].hospital_name}', 
-                    latlng: new kakao.maps.LatLng(${hospitals[i].latitude}, ${hospitals[i].longitude})
-            },`;
+            var H=" ";
+            var P=" ";
+            var D=" ";
+            var pet_list=``;
+            var day_list=``;
+            var count=0;
+            for (var i=0; i<Object.keys(hospitals).length-1;i++){
+                if(H!=hospitals[i].hospital_name){
+                    marker_list+=`{
+                        title: '${hospitals[i].hospital_name}', 
+                        latlng: new kakao.maps.LatLng(${hospitals[i].latitude}, ${hospitals[i].longitude}),
+                        night: ${hospitals[i].night},
+                        `;
+                    pet_list+=`${hospitals[i].pet},`;
+                    day_list+=`${hospitals[i].day}: ${hospitals[i].start_time}~${hospitals[i].end_time} <br>`;
+                    H=hospitals[i].hospital_name;
+                    P=hospitals[i].pet;
+                    D=hospitals[i].day;
+                }
+                else{
+                    if(P!=hospitals[i].pet){
+                        pet_list+=`${hospitals[i].pet}, `
+                        P=hospitals[i].pet;
+                    }
+                    if(D!=hospitals[i].day && count==0){
+                        day_list+=`${hospitals[i].day}: ${hospitals[i].start_time}~${hospitals[i].end_time} <br>`;
+                    }
+                    else{
+                        count++;
+                    }
+                }
+                if(H!=hospitals[i+1].hospital_name){
+                    marker_list+=`content:'<div><h6>${hospitals[i].hospital_name}<br>${pet_list}<br>${day_list}</h6></div>'},
+                    `;
+                if(i+1!=(hospitals).length){
+                    pet_list=` `;
+                    day_list=` `;
+                    count=0;
+                }
+                }
             }
+            marker_list+=`content:'<div><h6>${hospitals[i].hospital_name}<br>${pet_list}<br>${day_list}</h6></div>'},
+                `;
+            
+            res.end(main_template(marker_list));
         }
-        res.end(main_template(marker_list));
+        
     });
 });
 
