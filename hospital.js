c59f31d4ce4a5eb4e8679fab537843ac043fde0c
flora7883 <flora7883@gmail.com>
2021-12-03 21:51:55 +0000

    hospital_search

--- hospital.js
+++ hospital.js
@@ -9,7 +9,7 @@ app.use(body_parser.urlencoded({ extended: false}));
 var db = require('./db');
 const path=require('path')
 
-function main_template(marker_list){
+function main_template(marker_list,search_name){
     return `
     <!doctype html>
     <html>
@@ -18,8 +18,11 @@ function main_template(marker_list){
             <meta charset="utf-8">
         </head>
         <body> 
-        <h3>검색어를 입력하세요.</h3>
-        <h5>야간 운영 병원은 붉은색 마커로 표시됩니다.</h5>
+        <form action="/hospital/search?h_name=${search_name}"method="get">
+                <p><input type="text" name="search_name" placeholder="검색어를 입력하세요.">
+                <input type="submit" value="검색"></p>
+        </form>
+        <h6>야간 운영 병원은 붉은색 마커로 표시됩니다.</h6>
         <div id="map" style="width:700px;height:400px;"></div>
 	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=	e4410d501aa08cb040f89f52b5ee63d2"></script>
 	<script>
@@ -137,5 +140,60 @@ app.get('/', function (req, res) {
         
     });
 });
-
+app.get('/search/',function(req,res){
+    const keyword=req.query.search_name;
+    var marker_list=``;
+    console.log(keyword);
+    db.query(`SELECT * FROM hospital LEFT JOIN hospital_pet ON hospital.hospital_name=hospital_pet.hospital_name LEFT JOIN hospital_time ON hospital.hospital_name=hospital_time.hospital_name WHERE hospital.hospital_name LIKE ?`,['%'+keyword+'%'],function(err,hospitals){
+        if(err)throw err;
+        if(Object.keys(hospitals).length>0){
+            var H=" ";
+            var P=" ";
+            var D=" ";
+            var pet_list=``;
+            var day_list=``;
+            var count=0;
+            for (var i=0; i<Object.keys(hospitals).length-1;i++){
+                if(H!=hospitals[i].hospital_name){
+                    marker_list+=`{
+                        title: '${hospitals[i].hospital_name}', 
+                        latlng: new kakao.maps.LatLng(${hospitals[i].latitude}, ${hospitals[i].longitude}),
+                        night: ${hospitals[i].night},
+                        `;
+                    pet_list+=`${hospitals[i].pet},`;
+                    day_list+=`${hospitals[i].day}: ${hospitals[i].start_time}~${hospitals[i].end_time} <br>`;
+                    H=hospitals[i].hospital_name;
+                    P=hospitals[i].pet;
+                    D=hospitals[i].day;
+                }
+                else{
+                    if(P!=hospitals[i].pet){
+                        pet_list+=`${hospitals[i].pet}, `
+                        P=hospitals[i].pet;
+                    }
+                    if(D!=hospitals[i].day && count==0){
+                        day_list+=`${hospitals[i].day}: ${hospitals[i].start_time}~${hospitals[i].end_time} <br>`;
+                    }
+                    else{
+                        count++;
+                    }
+                }
+                if(H!=hospitals[i+1].hospital_name){
+                    marker_list+=`content:'<div><h6>${hospitals[i].hospital_name}<br>${pet_list}<br>${day_list}</h6></div>'},
+                    `;
+                if(i+1!=(hospitals).length){
+                    pet_list=` `;
+                    day_list=` `;
+                    count=0;
+                }
+                }
+            }
+            marker_list+=`content:'<div><h6>${hospitals[i].hospital_name}<br>${pet_list}<br>${day_list}</h6></div>'},
+                `;
+            
+            console.log(marker_list);
+            res.end(main_template(marker_list));
+        }
+    })
+});
 module.exports = app;
