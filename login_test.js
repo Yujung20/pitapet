3c334ef2890d9368701f5092b50ddd5557f31518
Jeon <jeonso0630@naver.com>
2021-12-18 00:28:10 +0900

    mail send with cron

--- login_test.js
+++ login_test.js
@@ -5,6 +5,8 @@ const body_parser = require('body-parser');
 app.use(body_parser.urlencoded({ extended: false}));
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-pool');
+const cron = require('node-cron');
 
 // router header add
 const register_router = require('./create_animal');
@@ -759,5 +761,68 @@ db.query(`SELECT * FROM user`, function(error, users) {
     console.log(users);
 })
 
+const transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+      user: '20171641@sungshin.ac.kr',
+      pass: 'pitapet!'
+    }
+  }));
+  
+  function send_mail(address, name, category) {
+    const mailOptions = {
+      from: '20171641@sungshin.ac.kr',
+      to: address,
+      subject: 'PitaPet에서 '+name+'의 '+category+'을 알려드립니다!',
+      text: name+'의 '+category+'입니다!'
+    };
+     
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
+  
+  //매일 오전 10시에 실행
+  cron.schedule('0 10 1-31 * *', function() {
+    var mail_number_array = [];
+    var owner_id_array = [];
+    var name_array = [];
+    var mail_category_array = [];
+    var now = new Date();
+    console.log(now);
+  
+    db.query('SELECT * FROM care_service WHERE care_service.mail_number IN (SELECT mail_number FROM care_service_date WHERE DATE_FORMAT(mail_date, "%Y-%m-%d") = CURDATE())', (error, rows1) => {
+      if (error) throw error;
+      else {
+        console.log("오늘 날짜인 케어 서비스\n");
+        for(let i = 0; i < rows1.length; i++){
+          mail_number_array[i] = rows1[i].mail_number;
+          owner_id_array[i] = rows1[i].owner_id;
+          name_array[i] = rows1[i].name;
+          mail_category_array[i] = rows1[i].mail_category;
+          console.log(mail_number_array[i], owner_id_array[i], name_array[i], mail_category_array[i]);
+        }
+      }
+  
+      db.query('SELECT * FROM user', (error, rows2) => {
+        if (error) throw error;
+        else {
+          for (var i=0; i<owner_id_array.length; i++) {
+            for (var j=0; j<rows2.length; j++) {
+              if (rows2[j].id == owner_id_array[i]) {
+                send_mail(rows2[j].email, name_array[i], mail_category_array[i]);
+              }
+            }
+          }
+        }
+      });
+  
+    });
+  });
 
 app.listen(80);
